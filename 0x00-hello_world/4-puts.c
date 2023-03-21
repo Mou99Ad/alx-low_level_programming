06ca72076c79986432990337a85f6db4207a6c49
Mou99Ad <mouad.elghanimi@gmail.com>
2023-03-21 02:26:21 +0000

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- * Description - Printing lines
- */
+*main - main entry point
+* return : 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"pogramming is like building a multilingual puzzle");
 	return (0);
 }
