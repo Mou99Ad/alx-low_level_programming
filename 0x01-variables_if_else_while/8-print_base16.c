809c800338136b6832c26b736a7af590097fa874
Mou99Ad <elgham-1999@hotmail.com>
2023-04-19 01:29:47 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 48; m <= 57 ; m++)
+	{
+		putchar(m);
+	}
+	for (n = 97; n <= 102 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+	}
