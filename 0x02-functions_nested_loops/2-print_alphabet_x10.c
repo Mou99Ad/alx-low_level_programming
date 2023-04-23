7ae34c892ccde3bc3598699201e0c4ad999bde38
Mou99Ad <elgham-1999@hotmail.com>
2023-04-23 23:41:44 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 	char letter;
 	int i;
 
-	for (letter = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		_putchar('\n');
+		i++;
 	}
 }
