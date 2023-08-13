89e407d5c4597dfa595b270df20ec803cccd714c
Mou99Ad <elgham-1999@hotmail.com>
2023-08-13 12:01:40 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry  main point
- * Description - c program uses the puts function to print a string followed by a newline.
- * return - 0 for success, non-zero for failure
+ * main - Entry point of the program
+ * Description : c program uses the puts function to print a string
+ * return : 0 for success, non-zero for failure
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
