b7071d228045f0ee0694836ca20c48f08909d81e
Mou99Ad <elgham-1999@hotmail.com>
2023-08-13 20:04:57 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - a C program that prints a line followed to the standard error.
-* fput - This is a description
-* Return: Always 1 (Error)
-*/
+ * main - a C program that prints a line followed to the standard error.
+ * Description : C program uses fputs followed by a new line
+ * Return: Always 1 (Error)
+ */
+
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
