4408ce9c933b1495f864a9f4e374660a3d28b1fb
Mou99Ad <elgham-1999@hotmail.com>
2023-08-13 12:14:09 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point of the program
  * Description : c program uses the puts function to print a string
- * return : Always 0 (success)
+ * Return: Always 0 (success)
 */
 
 int main(void)
