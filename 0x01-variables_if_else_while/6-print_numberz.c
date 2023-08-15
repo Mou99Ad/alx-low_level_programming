cc47f213b705d5410133682af04ceda2ca8b3ce5
Mou99Ad <elgham-1999@hotmail.com>
2023-08-15 13:24:28 +0100

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
+* Description: Prints single-digit numbers from 0 to 9 using putchar
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + '0');
+		putchar(i + '0'); /** Convert the integer value of 'i' to its ASCII character representation and print */
 	}
 	putchar('\n');
 	return (0);
