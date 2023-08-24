8d6877547d57e446f65e6724d08e2697612cced2
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-25 00:30:30 +0100

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Provided function to print a character */
+int _putchar(char c);
+/* Function to print a string followed by a new line recursively */
+void _puts_recursion(char *s);
+
+#endif
