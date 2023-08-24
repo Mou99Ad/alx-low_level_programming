ef13ff984505b6fd2d01cc63d8d3226918bddee2
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-25 00:34:57 +0100

    Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@
 int _putchar(char c);
 /* Function to print a string followed by a new line recursively */
 void _puts_recursion(char *s);
-
+/* Function to print a string in reverse recursively */
+void _print_rev_recursion(char *s);
 #endif
