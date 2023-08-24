51fc914fff4ff73861299ed6acb310e0643d56c7
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-24 17:29:26 +0100

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+int _atoi(char *s);
 
 
 #endif /* MAIN_H */
