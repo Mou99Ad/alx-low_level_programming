4d2a6eda7762aaa702f817e59ad3cc25c1e4c049
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-25 20:23:15 +0100

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings and returns 1 if they can be considered identical.
+* wildcmp - Compare 2 strings and returns 1 if they can be considered identical
  * @s1: The first string.
  * @s2: The second string.
  *
