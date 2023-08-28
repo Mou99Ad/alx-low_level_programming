886b169b929d689ffb68fbaebbfc03c88f83e425
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-28 23:56:25 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Represents information about a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+*/
 
 struct dog
 {
