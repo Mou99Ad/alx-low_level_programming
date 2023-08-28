09eafeae0f4d036ecb5187c3bf15cd4faa61c5b7
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-28 23:54:35 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
