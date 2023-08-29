e409cb800ce7634c101db60f14c176a05c33727e
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-29 14:00:08 +0100

    ITo hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	if (array && cmp && size > 0)
 	{
-		for (int i = 0; i < size; i++)
+		int i;
+
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
-				return i;
+				return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
