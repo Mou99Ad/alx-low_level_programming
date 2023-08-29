1d359501b3626589b5b0d772065d6ad9afaf56e6
Mou99Ad <mouad-elghanimi@hotmail.com>
2023-08-29 23:23:43 +0100

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, 1 if incorrect number of arguments, 2 if negative bytes
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *main_ptr = (unsigned char *)&main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+		if (i < bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
